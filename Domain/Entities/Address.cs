6e76a1734537741e4e6ce731e9a9ecefca11a7fb
HungNTDev <kmac08552@gmail.com>
2025-03-30 21:33:05 +0700

    Tạo các entity

---
+++ Domain/Entities/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public class Address : BaseEntity
+    {
+        public string? Unit_number { get; set; }
+        public string? Address_line1 { get; set; }
+        public string? City { get; set; }
+        public string? Region { get; set; }
+        //public ICollection<Order>? Orders { get; set; }
+        //public ICollection<UserAddress>? UserAddresses { get; set; }
+    }
+}
